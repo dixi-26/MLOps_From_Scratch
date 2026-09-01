f80cbd927ce4dd84790d787871b183fd2f343b77
dixi-26 <dixitays26@gmail.com>
2026-09-01 22:20:04 +0530

    Add files via upload

---
+++ day_01.py
@@ -0,0 +1,6 @@
+print("----MLOps target calculator----")
+user_name=input("Enter your name:")
+daily_hours=input("How many hours will you study per day?")
+total_yearly_hours=int(daily_hours)*365
+print("\nHello"+user_name+"!")
+print("At"+daily_hours+"hours a day,you will complete"+str(total_yearly_hours)+"hours of MLOps training this year!")
